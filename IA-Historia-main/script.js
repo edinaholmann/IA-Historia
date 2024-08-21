c29868cd7c835fd20065fd93a0631ec10802bb33
edina holmann <160506083+edinaholmann@users.noreply.github.com>
2024-08-21 17:42:00 -0300

    Add files via upload

--- IA-Historia-main/script.js
+++ IA-Historia-main/script.js
@@ -1,3 +1,21 @@
-const caixaPrincipal =document.querySelector(".caixa-principal");
-const caixaPerguntas=document.querySelector(".caixa-perguntas");
-const caixa
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado= document.querySelector(".texto-resultado");
+
+const perguntas =[
+    {
+        enunciado: "Um ponto positivo para a falta de professore nas escolas"
+        alternativas: [
+            {
+                texto: "isso é assustador"
+                afirmacao:"A falta de professores nas escolas sobrecarrega os educadores existentes, reduzindo a qualidade do ensino e o suporte individual para os alunos, o que pode levar a um desempenho acadêmico inferior."
+            }
+            {
+                texto:"isso é maravilhoso"
+                afirmacao:"A falta de professores pode incentivar a inovação no ensino e promover a autonomia dos alunos "
+            }
+        ]
+    }
+]
